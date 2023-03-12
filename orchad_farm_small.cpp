c8f9c15a0ff487935b6c8c3db823017ee9cf7dd6
kruti-29 <119089842+kruti-29@users.noreply.github.com>
2023-03-12 11:02:06 +0530

    Add files via upload

---
+++ orchad_farm_small.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input_small.txt", "r", stdin);
+    freopen("output_small.txt", "w", stdout);
+#endif
+
+    int t, income;
+    cin >> t;
+    while (t--)
+    {
+        int n, d;
+        cin >> n >> d;
+        int tree = n;
+        int arr[5][2] = {{12000, 10},
+                         {10000, 6},
+                         {27500, 15},
+                         {7500, 5},
+                         {8000, 15}};
+        vector<int> per_tree_income;
+        for (int i = 0; i < 5; i++)
+        {
+            int temp = arr[i][0] * (d / arr[i][1]);
+            per_tree_income.push_back(temp);
+            tree--;
+        }
+        income = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            income = income + per_tree_income.at(i);
+        }
+        // cout<<income <<endl;
+        sort(per_tree_income.begin(), per_tree_income.end(), greater<int>());
+
+        int max_capacity = (n * 0.4) - 1;
+        int i = 0;
+        while (tree > 0)
+        {
+            if (tree > max_capacity)
+            {
+                income = income + (per_tree_income.at(i) * max_capacity);
+                i++;
+                tree = tree - max_capacity;
+            }
+            else
+            {
+                income = income + (per_tree_income.at(i) * tree);
+                i++;
+                tree = tree - max_capacity;
+            }
+        }
+        /* for(int i=0;i<5;i++)
+        {
+            cout<<per_tree_income.at(i)<<endl;
+        } */
+        cout << income << endl;
+    }
+}
